18a7c26914aaad6e33cac3bd42366ff83c77743c
yao zhang <2940644288@qq.com>
2020-06-20 15:45:29 +0800

    add some new file

---
+++ ex1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"hello world"<<endl;
+	cout<<"i care you"<<endl;
+	return 1;
+}
---
+++ ex2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"i care you"<<endl;
+	return 1;
+}
