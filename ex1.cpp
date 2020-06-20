798e0a95d2dc2d983000c799a11250eaf2d0023a
yao zhang <2940644288@qq.com>
2020-06-20 15:48:47 +0800

    have change some lines

--- ex1.cpp
+++ ex1.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
 	cout<<"hello world"<<endl;
 	cout<<"i care you"<<endl;
+	cout<<"and i ofen miss you"<<endl;
 	return 1;
 }
--- ex2.cpp
+++ ex2.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main(){
 	cout<<"i care you"<<endl;
+	cout<<"mabe i have love you"<<endl;
+	cout<<"i believe my heart"<<endl;
 	return 1;
 }
